fe311a44453e05d66b61304140985678bfa58f59
Gautam Sood <gauty22@gmail.com>
2021-03-13 07:13:02 +0530

    Adding basic auth and import

---
+++ twitter_autoretweet_bot/tweet.py
@@ -0,0 +1,7 @@
+import tweepy as twitter
+import keys
+import time, datetime
+
+auth = twitter.OAuthHandler(keys.API_KEY, keys.API_SECRET_KEY)
+auth.set_access_token(keys.ACCESS_TOKEN, keys.SECRET_ACCESS_TOKEN)
+api = twitter.API(auth)
