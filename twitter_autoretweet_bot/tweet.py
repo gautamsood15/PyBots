eb570293a7e701bb02f8328ecfbfc1c14ef54ebd
Gautam Sood <gauty22@gmail.com>
2021-03-13 07:15:55 +0530

    Adding tweet function

--- twitter_autoretweet_bot/tweet.py
+++ twitter_autoretweet_bot/tweet.py
@@ -5,3 +5,12 @@ import time, datetime
 auth = twitter.OAuthHandler(keys.API_KEY, keys.API_SECRET_KEY)
 auth.set_access_token(keys.ACCESS_TOKEN, keys.SECRET_ACCESS_TOKEN)
 api = twitter.API(auth)
+
+
+
+
+def tweet(text):
+    api.update_status(text)
+
+tweet("Hello world this is a Twitter post!")
+
