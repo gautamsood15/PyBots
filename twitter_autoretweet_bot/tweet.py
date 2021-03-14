40d4d2d283003c9fe681a0fb0dd53504aa6717f2
Gautam Sood <gauty22@gmail.com>
2021-03-14 07:19:00 +0530

    Adding response function to tweet

--- twitter_autoretweet_bot/tweet.py
+++ twitter_autoretweet_bot/tweet.py
@@ -6,11 +6,11 @@ auth = twitter.OAuthHandler(keys.API_KEY, keys.API_SECRET_KEY)
 auth.set_access_token(keys.ACCESS_TOKEN, keys.SECRET_ACCESS_TOKEN)
 api = twitter.API(auth)
 
+id = 135124535732525734637
 
 
-
-def tweet(text):
-    api.update_status(text)
+def tweet(text, id):
+    api.update_status(text, id)                 # 2 arguments means that function responds to tweet, 1 argument mens its original tweet
 
 tweet("Hello world this is a Twitter post!")
 
