d66706cdbdea3020914c7ee269c123ae4da015e4
Gautam Sood <gauty22@gmail.com>
2021-03-18 10:36:02 +0530

    Adding code to locate heart on screen

---
+++ instagram_autolike_bot/main.py
@@ -0,0 +1,13 @@
+import pyautogui as pt
+from time import sleep
+
+# red heart colour = (237, 73, 86)
+
+class GuiCommands:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def navigate_to_heart(self, speed):
+        position = pt.locateOnScreen()
+
