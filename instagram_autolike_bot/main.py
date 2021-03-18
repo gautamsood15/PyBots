cf6d5ab1304ed02dca09d5c849cf1ca8f58218e8
Gautam Sood <gauty22@gmail.com>
2021-03-18 10:44:09 +0530

    Adding code to move to heart

--- instagram_autolike_bot/main.py
+++ instagram_autolike_bot/main.py
@@ -9,5 +9,9 @@ class GuiCommands:
         self.y = y
 
     def navigate_to_heart(self, speed):
-        position = pt.locateOnScreen()
-        
+        position = pt.locateOnScreen("bookmark.png", confidence=.8)
+        self.x = position[0] -550
+        self.y = position[1] + 10
+
+        pt.moveTo(self.x, self.y, duration=speed)
+
