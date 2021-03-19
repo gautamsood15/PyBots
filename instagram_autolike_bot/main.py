4bb6f051e12813aeabbb73bcadc41fb80a2d3fd4
Gautam Sood <gauty22@gmail.com>
2021-03-19 10:53:02 +0530

    FINAL COMMIT :)

--- instagram_autolike_bot/main.py
+++ instagram_autolike_bot/main.py
@@ -10,8 +10,25 @@ class GuiCommands:
 
     def navigate_to_heart(self, speed):
         position = pt.locateOnScreen("bookmark.png", confidence=.8)
-        self.x = position[0] -550
+        self.x = position[0] - 550
         self.y = position[1] + 10
-
         pt.moveTo(self.x, self.y, duration=speed)
+        print("Navigating to heart!")
+        sleep(.3)
 
+commands = GuiCommands(0, 0)
+
+for i in range(5):
+    try:
+        commands.navigate_to_heart(1)
+        if pt.pixelMatchesColor(pt.position().x, pt.position().y, (237, 73, 86), tolerance=10):
+            pt.scroll(5000)
+            sleep(.3)
+        else:
+            pt.click()
+            sleep(.3)
+
+    except Exception as e:
+        print(e)
+        pt.scroll(5000)
+        sleep(.3)
