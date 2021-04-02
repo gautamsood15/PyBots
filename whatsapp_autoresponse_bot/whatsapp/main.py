f07e888bc46358974eca65661698b13eadc1db14
Gautam Sood <gauty22@gmail.com>
2021-04-02 15:51:05 +0530

    Adding function to move the mouse pointer to smiley option

--- whatsapp_autoresponse_bot/whatsapp/main.py
+++ whatsapp_autoresponse_bot/whatsapp/main.py
@@ -5,8 +5,18 @@ import random
 
 sleep(3)
 
-position1 = pt.locateOnScreen("smily_paperclip.png", confidence=.6)
+position1 = pt.locateOnScreen("whatsapp/smiley_paperclip.png", confidence=.6)
 x = position1[0]
 y = position1[1]
 
 # Gets message
+def get_message():
+    global x, y
+
+    position = pt.locateOnScreen("whatsapp/smiley_paperclip.png", confidence=.6)
+    x = position[0]
+    y = position[1]
+
+    pt.moveTo(x, y, duration=0.5)
+
+get_message()
