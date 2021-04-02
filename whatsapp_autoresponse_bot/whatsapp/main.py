d93a924834bf991073cd43b10fdc604b8d1a2ca3
Gautam Sood <gauty22@gmail.com>
2021-04-02 15:45:04 +0530

    Adding detection of unread messages function

--- whatsapp_autoresponse_bot/whatsapp/main.py
+++ whatsapp_autoresponse_bot/whatsapp/main.py
@@ -1,4 +1,12 @@
 import pyautogui as pt
 from time import sleep
 import pyperclip
-import random 
+import random
+
+sleep(3)
+
+position1 = pt.locateOnScreen("smily_paperclip.png", confidence=.6)
+x = position1[0]
+y = position1[1]
+
+# Gets message
