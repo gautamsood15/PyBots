bde1ad95b54b9a3efe3838a23fcab947c8b0f401
Gautam Sood <gauty22@gmail.com>
2021-04-04 03:31:05 +0530

    Adding screenposition capture image

--- whatsapp_autoresponse_bot/whatsapp/colours_and_positions.py
+++ whatsapp_autoresponse_bot/whatsapp/colours_and_positions.py
@@ -3,5 +3,11 @@ from time import sleep
 
 
 while True:
-    pass
+    posXY = pt.position()
+    print(posXY, pt.pixel(posXY[0], posXY[1]))
+    sleep(1)
+
+    if posXY[0] == 0:
+        break
+
 
